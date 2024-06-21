20953e34d06e7ca664abf2ae7192c1014d65ac67
szlishchev <stanislavz@ext.inditex.com>
2024-06-21 08:42:48 +0300

    completed rest template testing

--- src/test/java/guru/springframework/spring6resttemplate/clients/impl/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/clients/impl/BeerClientMockTest.java
@@ -18,14 +18,19 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withNoContent;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withResourceNotFound;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RestClientTest
@@ -63,7 +68,7 @@ public class BeerClientMockTest {
     }
 
     @Test
-    void testListBeerMock() throws JsonProcessingException {
+    void testListBeer() throws JsonProcessingException {
         final var payload = this.objectMapper.writeValueAsString(this.getPage());
         
         mockRestServiceServer.expect(method(HttpMethod.GET))
@@ -73,6 +78,25 @@ public class BeerClientMockTest {
         final var beerList = this.beerClient.getBeerList();
         
         assertThat(beerList.getSize()).isGreaterThan(0);
+    }
+
+    @Test
+    void testListBeerWithQuery() throws JsonProcessingException {
+        final var payload = this.objectMapper.writeValueAsString(this.getPage());
+        
+        final var uri = UriComponentsBuilder.fromUriString(URL + BeerClientImpl.BEER_PATH)
+            .queryParam("beerName", dto.getBeerName())
+            .build().toUri();
+        
+        this.mockRestServiceServer.expect(method(HttpMethod.GET))
+            .andExpect(requestTo(uri))
+            .andExpect(queryParam("beerName", dto.getBeerName()))
+            .andRespond(withSuccess(payload, MediaType.APPLICATION_JSON));
+        
+        final var response = this.beerClient.getBeerList(dto.getBeerName(), null, null, null,null);
+        
+        assertThat(response).isNotNull();
+        assertThat(response.getSize()).isGreaterThan(0);
     }
 
     @Test
@@ -100,7 +124,7 @@ public class BeerClientMockTest {
     @Test
     void testUpdateBeer() {
         mockRestServiceServer.expect(method(HttpMethod.PUT))
-                .andExpect(requestToUriTemplate(URL + BeerClientImpl.BEER_PATH, dto.getId()))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.BEER_BY_ID_PATH, dto.getId()))
                 .andRespond(withSuccess(jsonDto, MediaType.APPLICATION_JSON));
         
         runGetOperation();
@@ -111,10 +135,34 @@ public class BeerClientMockTest {
         assertThat(updateBeer.getId()).isEqualTo(dto.getId());
     }
 
+    @Test
+    void testDeleteBeer() {
+        mockRestServiceServer.expect(method(HttpMethod.DELETE))
+            .andExpect(requestToUriTemplate(URL + BeerClientImpl.BEER_BY_ID_PATH, dto.getId()))
+            .andRespond(withNoContent());
+        
+        this.beerClient.deleteBeer(dto.getId());
+        
+        mockRestServiceServer.verify();
+    }
+
+    @Test
+    void testDeleteBeerNotFound() {
+        mockRestServiceServer.expect(method(HttpMethod.DELETE))
+            .andExpect(requestToUriTemplate(URL + BeerClientImpl.BEER_BY_ID_PATH, dto.getId()))
+            .andRespond(withResourceNotFound());
+
+        assertThrows(HttpClientErrorException.class, () -> this.beerClient.deleteBeer(dto.getId()));
+        
+        mockRestServiceServer.verify();
+    }
+    
+    
+
     private BeerDTO getBeerDTO() {
         return BeerDTO.builder()
                 .id(UUID.randomUUID())
-                .beerName("Test Beer")
+                .beerName("Test")
                 .beerStyle(BeerStyle.ALE)
                 .upc("2421")
                 .price(BigDecimal.TEN)
